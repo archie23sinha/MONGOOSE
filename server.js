fa25db339d465295493c1ae8a2b18a8b0ae5a098
Archie <archie23sinha@gmail.com>
2024-07-13 13:04:31 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const PORT = 5000 || process.env.PORT
 const app =express();
-const URL ='mongodb://localhost:27017/students-database';
+const URL ='mongodb+srv://archie23sinha:bKE8HghaUOCKcnea@first.ed2irjj.mongodb.net/students-database';
 const connectToDB = async()=>
 {
   try 
@@ -40,7 +40,60 @@ const UserProfileSchema = new mongoose.Schema({
 //COMPILE THE SHEMA TO FORM THE MODEL
 
 const User = mongoose.model ('User' , UserProfileSchema);
-//CREATE A NEW USER
+
+//CRUD OPERATIONS
+
+//CREATE A DOC - .save()
+
+const newUser = new User ({
+  username : 'Archie' ,
+  age : 23 ,
+  birthday : new Date ('2000-10-23') ,
+  isActive : true ,
+  hobbies : ['travelling' , 'music' , 'coding'] ,
+  objectId : new mongoose.Types.ObjectId() ,
+  address : {
+    street : 'Saheban Hata' ,
+    city : 'Purnia' ,
+    postCode : 854301 ,
+    },
+    customData : {
+      country  : 'India' ,
+      
+      },
+      });
+//       newUser.save().then((data)=>
+//       {
+// console.log(data);
+//       }) 
+//       .catch((e) => console.log(e));
+
+//CREATE
+
+User.create({
+  username : 'Vansh' ,
+  age : 23 ,
+  birthday : new Date ('2000-10-23') ,
+  isActive : true ,
+  hobbies : ['travelling' , 'music' , 'coding'] ,
+  objectId : new mongoose.Types.ObjectId() ,
+  address : {
+    street : 'Saheban Hata' ,
+    city : 'Purnia' ,
+    postCode : 854301 ,
+    },
+    customData : {
+      country  : 'India' ,
+      
+      },
+      })
+      .then((data)=>
+      {
+console.log(data);
+      }) 
+      .catch((e) => console.log(e));
+
+
 
 
 app.listen(PORT,console.log(`Server is running at port ${PORT}`));
